e8b0d936179377ba7bae36122d8e98239d8056bc
Tien Han <tienthuyhan@gmail.com>
2024-04-11 13:27:03 -0400

    Add controller and default route

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 328/pets/index.php
+ * Simple MVC using the Fat-Free framework.
+ * @author Tien Han
+ * @version 1.0
+ */
+
+    //Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    //Require the autoload file
+    require_once('vendor/autoload.php');
+
+    //Instantiate the F3 Base class (Fat-Free)
+    $f3 = Base::instance();
+
+    //Define a default route
+    $f3-> route('GET /', function() {
+        //Render a view page
+        echo "Pet Home";
+    });
+
+    //Run Fat-Free
+    $f3->run();
+?>
